2113e0a36fe85e5b2705c9db1e10ecf4fe20238e
harold-voces <harold@vdlf.org>
2024-11-01 19:55:01 +0000

    Updated vercel.json

--- api/email-api.js
+++ api/email-api.js
@@ -1,38 +1,51 @@
 const nodemailer = require('nodemailer');
 
+// export default async function handler(req: VercelRequest, res: VercelResponse) {
+   
+
+//     return res.json({ message: 'Hello World' });
+//   }
+
 const handler = async (req, res) => {
+
+    if (req.method === 'OPTIONS') {
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, X-CSRF-Token, X-Api-Version');
+        return res.status(200).send('');
+      }
 
   console.log('Server log here');
 
-  const { staffEmail, name, email, message } = req.body;
+//   const { staffEmail, name, email, message } = req.body;
 
-  if (!staffEmail || !name || !email || !message) {
-    return res.status(400).json({ error: 'All fields are required.' });
-  }
+//   if (!staffEmail || !name || !email || !message) {
+//     return res.status(400).json({ error: 'All fields are required.' });
+//   }
 
-  try {
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       service: 'Gmail',
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
 
-    const mailOptions = {
-      from: email,
-      to: staffEmail,
-      subject: `Contact from ${name}`,
-      text: message,
-    };
+//     const mailOptions = {
+//       from: email,
+//       to: staffEmail,
+//       subject: `Contact from ${name}`,
+//       text: message,
+//     };
 
-    await transporter.sendMail(mailOptions);
+//     await transporter.sendMail(mailOptions);
 
-    res.status(200).json({ message: 'Email sent successfully' });
-  } catch (error) {
-    console.error('Error sending email:', error);
-    res.status(500).json({ error: 'Failed to send email' });
-  }
+//     res.status(200).json({ message: 'Email sent successfully' });
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     res.status(500).json({ error: 'Failed to send email' });
+//   }
 };
 
 export default handler;
