4ebd8b0fbe27b0d466589c5a8fb17676e90f5fe1
harold-voces <harold@vdlf.org>
2024-11-01 19:55:33 +0000

    Updated vercel.json

--- api/email-api.js
+++ api/email-api.js
@@ -12,7 +12,7 @@ const handler = async (req, res) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept, X-CSRF-Token, X-Api-Version');
-        return res.status(200).send('');
+        return res.status(200).send('Hola');
       }
 
   console.log('Server log here');
