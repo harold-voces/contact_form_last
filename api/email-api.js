5b6c253ec7b974f986b891cc5183aa732e236589
harold-voces <harold@vdlf.org>
2024-11-01 20:13:20 +0000

    Updated vercel.json

--- api/email-api.js
+++ api/email-api.js
@@ -17,35 +17,35 @@ const handler = async (req, res) => {
 
   console.log('Server log here');
 
-//   const { staffEmail, name, email, message } = req.body;
+  const { staffEmail, name, email, message } = req.body;
 
-//   if (!staffEmail || !name || !email || !message) {
-//     return res.status(400).json({ error: 'All fields are required.' });
-//   }
+  if (!staffEmail || !name || !email || !message) {
+    return res.status(400).json({ error: 'All fields are required.' });
+  }
 
-//   try {
-//     const transporter = nodemailer.createTransport({
-//       service: 'Gmail',
-//       auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS,
-//       },
-//     });
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
 
-//     const mailOptions = {
-//       from: email,
-//       to: staffEmail,
-//       subject: `Contact from ${name}`,
-//       text: message,
-//     };
+    const mailOptions = {
+      from: email,
+      to: staffEmail,
+      subject: `Contact from ${name}`,
+      text: message,
+    };
 
-//     await transporter.sendMail(mailOptions);
+    await transporter.sendMail(mailOptions);
 
-//     res.status(200).json({ message: 'Email sent successfully' });
-//   } catch (error) {
-//     console.error('Error sending email:', error);
-//     res.status(500).json({ error: 'Failed to send email' });
-//   }
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Failed to send email' });
+  }
 };
 
 export default handler;
