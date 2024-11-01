b705e0c6f98e6ea7f9f071ecacda2b40e0aa0136
harold-voces <harold@vdlf.org>
2024-11-01 18:34:35 +0000

    Add port

---
+++ api/email-api.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer');
+
+const handler = async (req, res) => {
+
+  if (req.method !== 'POST') {
+    res.status(405).json({ error: 'Method not allowed' });
+    return;
+  }
+
+  const { staffEmail, name, email, message } = req.body;
+
+  if (!staffEmail || !name || !email || !message) {
+    return res.status(400).json({ error: 'All fields are required.' });
+  }
+
+  try {
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+
+    const mailOptions = {
+      from: email,
+      to: staffEmail,
+      subject: `Contact from ${name}`,
+      text: message,
+    };
+
+    await transporter.sendMail(mailOptions);
+
+    res.status(200).json({ message: 'Email sent successfully' });
+  } catch (error) {
+    console.error('Error sending email:', error);
+    res.status(500).json({ error: 'Failed to send email' });
+  }
+};
+
+export default handler;
